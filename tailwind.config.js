7f69419086374fdad5c76e12757825f250f57405
Manoj Bahuguna <manojbahugunamanoj@gmail.com>
2022-04-03 16:10:55 +0530

    chore(tailwind): adds colors and container config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,29 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      lightPrimary: "#FEF6E4",
+      lightSecondary: "#F3D2C1",
+      darkPrimary: "#001858",
+      darkSecondary: "#172C66",
+      accentLight: "#F582AE",
+      white: "white",
+      transparent: "transparent",
+      current: "currentColor",
+      black: "black",
+    },
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "2rem",
+          xl: "7rem",
+          "2xl": "10rem",
+        },
+      },
+    },
   },
   plugins: [],
 };
