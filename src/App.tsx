eabad6ab1fdecb0e008607704ed18843c6f87330
Manoj Bahuguna <manojbahugunamanoj@gmail.com>
2023-01-12 17:47:59 +0530

    feat: add Contact section

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import { Hero } from "./sections/Hero";
 import { Experiences } from "./sections/Experiences";
 import { Recommendations } from "./sections/Recommendations";
 import { SecondaryCursor } from "./utils/SecondaryCursor";
+import { Contact } from "./sections/Contact";
 
 export function App() {
   return (
-    <div className="bg-[url(/grainy-gradient.svg)] bg-fixed">
+    <div className="bg-[url(/grainy-gradient.svg)]">
       <SecondaryCursor />
       <div className="relative z-20 bg-[url(/grainy-gradient.svg)]">
         <Hero />
         <Experiences />
         <Recommendations />
+        <Contact />
       </div>
     </div>
   );
---
+++ src/sections/Contact.tsx
@@ -0,0 +1,65 @@
+const links = [
+  { title: "LinkedIn", href: "https://linkedin.com/in/BahugunaManoj" },
+  { title: "Github", href: "https://github.com/ManojBahuguna" },
+  { title: "CodePen", href: "https://codepen.io/ManojBahuguna" },
+  { title: "Toptal", href: "https://toptal.com/resume/manoj-bahuguna" },
+];
+
+export function Contact() {
+  return (
+    <div id="contact" className="bg-red bg-opacity-70">
+      <div className="container py-24 sm:py-32">
+        <h2 className="text-white mb-20">
+          <small className="uppercase">Ways to </small>
+          <span className="FontFancy text-3xl sm:text-5xl font-bold block">
+            Connect
+          </span>
+          <small className="uppercase"> with me</small>
+        </h2>
+
+        <ul className="grid gap-2 text-accent place-items-start my-12">
+          {links.map(({ title, href }) => (
+            <li key={title}>
+              <a
+                className="text-xl sm:text-3xl block bg-accent text-white font-light py-2 px-6 rounded-sm transition-transform hover:translate-x-1"
+                href={href}
+                target="_blank"
+                rel="noreferrer"
+              >
+                {title} &#8599;
+              </a>
+            </li>
+          ))}
+        </ul>
+
+        <div>
+          <p>
+            <a
+              className="inline-block text-lg sm:text-2xl text-lightPrimary hover:text-white my-2 underline decoration-dotted decoration-4 break-all"
+              href="mailto:manojbahugunamanoj@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              manojbahugunamanoj@gmail.com
+            </a>
+          </p>
+
+          <p>
+            <a
+              className="inline-block text-lg sm:text-2xl text-lightPrimary hover:text-white my-2 underline decoration-dotted decoration-4"
+              href="tel:+91-8586838473"
+              target="_blank"
+              rel="noreferrer"
+            >
+              +91 8586 8384 73
+            </a>
+          </p>
+
+          <p className="text-white text-opacity-70 text-lg my-4">
+            New Delhi, India
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
