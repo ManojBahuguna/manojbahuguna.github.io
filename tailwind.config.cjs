2d07af97c869e3e498eb562f9e2336c805155fc1
Manoj Bahuguna <manojbahugunamanoj@gmail.com>
2023-05-23 10:32:02 +0530

    feat: setup PWA

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const colors = require("tailwindcss/colors");
 
 module.exports = {
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { type ManifestOptions, VitePWA } from "vite-plugin-pwa";
+
+const manifest: Partial<ManifestOptions> = {
+  name: "Manoj Bahuguna's Personal Website",
+  short_name: "Manoj Bahuguna",
+  icons: [
+    { src: "/android-chrome-192x192.png", sizes: "192x192", type: "image/png" },
+    { src: "/android-chrome-512x512.png", sizes: "512x512", type: "image/png" },
+  ],
+  theme_color: "#FEF6E4",
+  background_color: "#FEF6E4",
+  display: "standalone",
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest,
+      workbox: {
+        navigateFallbackAllowlist: [], // app routes will go here if added
+      },
+    }),
+  ],
 });
